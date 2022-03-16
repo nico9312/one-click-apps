a27f4f1386de33ce8053ade75c82a24032fe91d4
Nico Kowalczyk <nico.kowalczyk@googlemail.com>
2022-03-16 11:18:12 +0100

    corrected publish

--- scripts/publish-from-actions.sh
+++ scripts/publish-from-actions.sh
@@ -82,8 +82,8 @@ echo "#############################################"
 echo "######### Commit and push ###" 
 echo "#############################################" 
 sleep 1s
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config user.name "nico9312"
+git config user.email "nico9312@users.noreply.github.com"
 echo `date` >> forcebuild.date
 git add -A 
 git commit -m 'Deploy to GitHub Pages' 
